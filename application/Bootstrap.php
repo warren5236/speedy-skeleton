3792bc4121a792582c90e8db47052ca9b618465f
Scott Warren <warren5236@gmail.com>
2012-08-26 20:25:00 -0400

    Integrated feature toggles.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,17 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+	protected function _initFeatureToggles(){
+		
+		// initialize the feature toggle
+		$featuretoggle = Speedy_Featuretoggle::getInstance();
+		
+		$settings = array();
+		foreach(Speedy_ModuleHelper::getAllModuleFiles('settings/features.php') as $file){
+			$settings = array_merge_recursive($settings, eval(file_get_contents($file)));
+		}
+		
+		$featuretoggle->setSettings($settings);
+	}
 }
 
---
+++ application/modules/default/settings/features.php
@@ -0,0 +1,24 @@
+return array(
+	'siteheader' => array(
+		'description'=>'Site specific header information',
+		'enabled' => false,
+		'sites'=>array(
+			'development' => array(
+				'startDate'=>'2012-08-26'
+			)
+		)
+	),
+	'help' => array(
+		'description'=>'The help module',
+		'enabled' => false
+	),
+	'user' => array(
+		'description'=>'The user module',
+		'enabled' => false,
+		'sites'=>array(
+			'development' => array(
+				'startDate'=>'2012-09-01'
+			)
+		)
+	)
+);
