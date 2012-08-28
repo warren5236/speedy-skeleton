0654258b9e47ec2c9010ee756de5b38beeb44b55
Scott Warren <warren5236@gmail.com>
2012-08-27 21:45:19 -0400

    Added support to check the modules needed for the site.

--- application/modules/deployment/models/tasks/Healthcheck.php
+++ application/modules/deployment/models/tasks/Healthcheck.php
@@ -6,8 +6,51 @@ class Deployment_Model_Tasks_Healthcheck extends Deployment_Model_Tasks_Generic{
 	
 	protected $_autoRun = true;
 	
+	protected $_modules = array(
+		'required'=>array('mod_rewrite'=>'This module is so required I\'m not even sure how you\'re seeing this message.'),
+		'nicetohave'=>array(
+				'mod_headers'=>'Provides support to have long lived static files.',
+				'mod_expires'=>'Improves long lived static files',
+				'mod_gzip'=>'Compresses static files'
+		)
+	);
+	
+	public function _checkModules(){
+		
+		$this->_addResult('## Module Check');
+		
+		// get the currently loaded modules
+		$modules = apache_get_modules();
+		
+		// the return value
+		$returnVal = true;
+		
+		// loop through all the required modules
+		foreach($this->_modules['required'] as $module => $description){
+			// check to see if this module
+			if(!in_array($module, $modules)){
+				$this->_addResult('* Missing required module:' . $module);
+				$returnVal = false;
+			}
+		}
+		
+		// loop through all the required modules
+		foreach($this->_modules['nicetohave'] as $module => $description){
+			// check to see if this module
+			if(!in_array($module, $modules)){
+				$this->_addResult('* Missing optional module:' . $module);
+			}
+		}
+		
+		return $returnVal;
+	}
+	
 	public function runTask(){
-		$returnVal = array();
+		$returnVal = true;
+		
+		if(!$this->_checkModules()){
+			$returnVal = false;
+		}
 		
 		return $returnVal;
 	}
